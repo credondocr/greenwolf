724a7278af368fd37eeea9bbc1841db4d51b998c
Cesar Redondo <cesar.redondo@edify.cr>
2019-08-13 21:19:04 -0600

    Text change

--- src/components/Main.js
+++ src/components/Main.js
@@ -30,9 +30,9 @@ class Main extends React.Component {
         </article>
 
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Preguntas Frecuentes</h2>
+          <h2 className="major">FAQ</h2>
           <span className="image main"><img src={pic03} alt="" /></span>
-          <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+          <p>Recopilando información, próximamente publicaremos las preguntas, gracias por su paciencia.</p>
           {close}
         </article>
 
